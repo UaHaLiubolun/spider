9672eb29a76a5bedd667ee14e237ea067a2e808f
SignalDog <liubolunliuyao@gmail.com>
2018-05-29 10:18:21 +0800

    set sleep time

--- core/src/main/java/spider/filter/example/netease/Main.java
+++ core/src/main/java/spider/filter/example/netease/Main.java
@@ -12,6 +12,7 @@ public class Main {
     public static void main(String[] args) {
         Site site = new Site();
         site.setDomain("music.163.com");
+        site.setSleepTime(2000);
         Request request = new Request("http://music.163.com/discover/playlist", new CatFilter());
         List<Request> requests = new ArrayList<Request>();
         requests.add(request);
